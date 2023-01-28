4cab7166ad17c2ac4512049cc41d85cfca7e4347
Igor Teplych <igorvrt@mail.ru>
2023-01-28 23:14:14 +0600

    Добавил тестовый проект

---
+++ SubstringSearch.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SubstringSearch.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, false);
+        }
+    }
+}
