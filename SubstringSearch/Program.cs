83a48d5768b8dd4305f15d6f872156486da8719d
Igor Teplych <igorvrt@mail.ru>
2023-01-28 23:09:33 +0600

    Реализовал алгоритм грубой силы и Бойера-Мура

--- SubstringSearch/Program.cs
+++ SubstringSearch/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using SubstringSearch;
+
+public static class Programm
+{
+    public static void Main()
+    {
+        string s1 = "strongstring";//"большие метеоданные";//
+        string s2 = "rin";//"данные";//
+        Substring substring = new Substring();
+        var vr = substring.SearchSubstringByBrutalForce(s1, s2);
+        var vr1 = substring.SearchBoyerMoore(s1, s2);
+    }
+}
