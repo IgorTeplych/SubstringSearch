f2852e9a30d6afb31237a0cd0633e69b93276e99
Igor Teplych <igorvrt@mail.ru>
2023-02-08 22:22:02 +0600

    Добавил тест поиска строки в тексте с часто повторяющимися символами.

--- SubstringSearch/Program.cs
+++ SubstringSearch/Program.cs
@@ -5,7 +5,14 @@ using System.Text;
 public static class Programm
 {
     static int T = 1000;
+    static string sub = "Twenty men were convicted";
     public static void Main()
+    {
+        //SearchInBook();
+        SearchInText();
+    }
+
+    static void SearchInBook()
     {
         string text = GetText();
         string substring = "Twenty men were convicted";
@@ -21,23 +28,16 @@ public static class Programm
         Console.WriteLine($"Поиск строки, длиной {substring.Length}, в тексте, длиной {text.Length}, методом Бойера-Мура занимает {Search(search.SearchBoyerMoore, text, substring)} мс.");
         Console.ReadLine();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    static void SearchInText()
+    {
+        string substring = "CaCaCCaCaCCaCaCCaCaCCaCa";
+        string text = GenerateText(substring, "aaBaaaBaBaBaa");
+        Substring search = new Substring();
+        Console.WriteLine("ПОИСК СТРОКИ, КОТОРАЯ ВСТРЕЧАЕТСЯ В ТЕКСТЕ");
+        Console.WriteLine($"Поиск строки, длиной {substring.Length}, в тексте, длиной {text.Length}, методом полного перебора занимает {Search(search.SearchSubstringByBrutalForce, text, substring)} мс.");
+        Console.WriteLine($"Поиск строки, длиной {substring.Length}, в тексте, длиной {text.Length}, методом Бойера-Мура занимает {Search(search.SearchBoyerMoore, text, substring)} мс.");
+        Console.ReadLine();
+    }
     static long Search(Func<string, string, int> searchMethod, string text, string substring)
     {
         Stopwatch sw = new Stopwatch();
@@ -55,5 +55,24 @@ public static class Programm
     {
         string path = Environment.CurrentDirectory + @"\text.doc";
         return File.ReadAllText(path);
+    }
+    static string GenerateText(string substring, string agr)
+    {
+        string s = "";
+        string text = GetText();
+        Substring search = new Substring();
+        var pos = search.SearchBoyerMoore(text, sub);
+        bool wr = false;
+        while (s.Length < text.Length)
+        {
+            if (s.Length >= pos && !wr)
+            {
+                s += substring;
+                wr = true;
+            }
+            s += agr;
+        }
+
+        return s;
     }
 }
